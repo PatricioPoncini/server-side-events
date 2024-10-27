36959c2647043aff5931e13e3b30ca89ebd9d902
PatricioPoncini <patricioponcini10@gmail.com>
2024-10-27 12:58:40 -0300

    feat: add logger, improve styles in index.html, and enhance server events

--- main.go
+++ main.go
@@ -10,11 +10,35 @@ import (
 	"github.com/shirou/gopsutil/v4/mem"
 )
 
+const (
+	colorReset  = "\033[0m"
+	colorRed    = "\033[31m"
+	colorYellow = "\033[33m"
+	colorCyan   = "\033[36m"
+)
+
+func colorLog(level, message string) {
+	var color string
+	switch level {
+	case "INFO":
+		color = colorCyan
+	case "WARN":
+		color = colorYellow
+	case "ERROR":
+		color = colorRed
+	default:
+		color = colorReset
+	}
+	log.Printf("%s[%s] %s%s", color, level, message, colorReset)
+}
+
 func main() {
 	http.HandleFunc("/events", sseHandler)
 
+	colorLog("INFO", "Server started and listening on port :8080")
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("unable to start server: %s", err.Error())
+		colorLog("ERROR", fmt.Sprintf("unable to start server: %s", err.Error()))
 	}
 }
 
@@ -37,26 +61,43 @@ func sseHandler(w http.ResponseWriter, r *http.Request) {
 	for {
 		select {
 		case <-clientGone:
-			fmt.Println("client has disconnected") // TODO: Improve this with colors, better logger
+			colorLog("INFO", fmt.Sprintf("client has disconnected"))
+			return
 		case <-memT.C:
 			m, err := mem.VirtualMemory()
 			if err != nil {
-				log.Printf("unable to get mem: %s", err.Error())
+				colorLog("ERROR", fmt.Sprintf("unable to get mem: %s", err.Error()))
 				return
 			}
-			if _, err := fmt.Fprintf(w, "event:mem\ndata:Total: %d, Used: %d, Perc: %.2f%%\n\n", m.Total, m.Used, m.UsedPercent); err != nil {
-				log.Printf("unable to write: %s", err.Error())
+			if _, err := fmt.Fprintf(w, "event:memTotal\ndata:%d\n\n", m.Total); err != nil {
+				colorLog("ERROR", fmt.Sprintf("unable to write memTotal: %s", err.Error()))
+				return
+			}
+			if _, err := fmt.Fprintf(w, "event:memUsed\ndata:%d\n\n", m.Used); err != nil {
+				colorLog("ERROR", fmt.Sprintf("unable to write memUsed: %s", err.Error()))
+				return
+			}
+			if _, err := fmt.Fprintf(w, "event:memPerc\ndata:%.2f%%\n\n", m.UsedPercent); err != nil {
+				colorLog("ERROR", fmt.Sprintf("unable to write memPerc: %s", err.Error()))
 				return
 			}
 			rc.Flush()
 		case <-cpuT.C:
 			c, err := cpu.Times(false)
 			if err != nil {
-				log.Printf("unable to get cpu: %s", err.Error())
+				colorLog("ERROR", fmt.Sprintf("unable to get cpu: %s", err.Error()))
 				return
 			}
-			if _, err := fmt.Fprintf(w, "event:cpu\ndata:User: %.2f, Sys: %.2f, Idle: %.2f\n\n", c[0].User, c[0].System, c[0].Idle); err != nil {
-				log.Printf("unable to write: %s", err.Error())
+			if _, err := fmt.Fprintf(w, "event:cpuUser\ndata:%.2f\n\n", c[0].User); err != nil {
+				colorLog("ERROR", fmt.Sprintf("unable to write cpuUser: %s", err.Error()))
+				return
+			}
+			if _, err := fmt.Fprintf(w, "event:cpuSys\ndata:%.2f\n\n", c[0].System); err != nil {
+				colorLog("ERROR", fmt.Sprintf("unable to write cpuSys: %s", err.Error()))
+				return
+			}
+			if _, err := fmt.Fprintf(w, "event:cpuIdle\ndata:%.2f\n\n", c[0].Idle); err != nil {
+				colorLog("ERROR", fmt.Sprintf("unable to write cpuIdle: %s", err.Error()))
 				return
 			}
 			rc.Flush()
